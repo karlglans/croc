41c40d912bec9c5144f1b65098681a121335af67
karlglans <karl_glans@hotmail.com>
2019-05-29 14:37:23 +0200

    added another survey to testset

--- src/test/java/se/purple/croc/graphql/QuestionIT.java
+++ src/test/java/se/purple/croc/graphql/QuestionIT.java
@@ -12,14 +12,15 @@ public class QuestionIT extends SimpleEndpointTests {
 	@Test
 	public void canAddQuestion() {
 		String json = excQuery("mutation { addQuestion( input: {text: \"aaa\"} ){ id }}");
-		String expected = "{\"addQuestion\":{\"id\":\"5\"}}";
+		String expected = "{\"addQuestion\":{\"id\":\"9\"}}";
 		assertEquals(expected, json);
 	}
 
 	@Test
 	public void canAlterQuestion() {
 		String json = excQuery("mutation { updateQuestion( input: {text: \"aaa\", id: 3} ){ id questions { id text } }}");
-		String expected = "{\"updateQuestion\":{\"id\":\"2\",\"questions\":[{\"id\":\"2\",\"text\":\"question2 form1 and form2\"},{\"id\":\"3\",\"text\":\"aaa\"}]}}";
+		String expected = "{\"updateQuestion\":{\"id\":\"2\"," +
+				"\"questions\":[{\"id\":\"2\",\"text\":\"question2 form1 and form2\"},{\"id\":\"3\",\"text\":\"aaa\"}]}}";
 		assertEquals(expected, json);
 	}
 }
