c22d1ced1cb24511d8c9462322dbbd22a9d14f79
karlglans <karl_glans@hotmail.com>
2019-07-03 17:09:19 +0200

    bugfix: connecting users to survey

--- src/main/frontend3/src/pages/AdminPage/SurveySubPage/components/AddParticipants/HandleAddParticipants.jsx
+++ src/main/frontend3/src/pages/AdminPage/SurveySubPage/components/AddParticipants/HandleAddParticipants.jsx
@@ -15,6 +15,7 @@ const ADD_USERGROUP_TO_SURVEY = gql`
 const GET_PARTICPANT_DATA = gql`
   query($surveyId: ID) {
     survey(id: $surveyId) {
+      id
       participants {
         id
         email
